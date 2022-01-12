183f78cc732afde82d015d8c58e42f737fc9c054
zcsryu2 <zcsryu2@126.com>
2022-01-12 20:15:51 +0800

    day one

---
+++ LAB/lab00/lab00.py
@@ -0,0 +1,8 @@
+def twenty_nineteen():
+    """Come up with the most creative expression that evaluates to 2019,
+    using only numbers and the +, *, and - operators.
+
+    >>> twenty_nineteen()
+    2019
+    """
+    return -(1-145)*14+(11*-4+51-4)
