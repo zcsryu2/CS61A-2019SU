bcabdc7cefb87b9fde9ac6d3c4f8c439eca94ec1
zcsryu2 <zcsryu2@126.com>
2022-06-07 23:39:58 +0800

    2022

--- PROJECT/typing_test/typing_test.py
+++ PROJECT/typing_test/typing_test.py
@@ -17,6 +17,45 @@ def new_sample(path, i):
     line = lines_from_file(path)
     return line[i]
 
+def analyze(sample_paragraph, typed_string, start_time, end_time):
+    lst = []
+
+    words_num = len(typed_string) / 5
+    v = words_num * 60 / (end_time - start_time)
+    lst.append(v)
+
+    lst0 = split(sample_paragraph)
+    lst1 = split(typed_string)
+    correct_num = 0
+    if len(lst0) > len(lst1):
+        length = len(lst1)
+    else:
+        length = len(lst0)
+    for i in range(length):
+        if lst0[i] == lst1[i]:
+            correct_num += 1
+    if length == 0:
+        percent = 0.0
+    else:
+        percent = correct_num * 100 / length
+    lst.append(percent)
+    return lst
+
+def pig_latin(words):
+    i = 0
+    for i in range(len(words)):
+        if words[i] in "aoeiu":
+            break
+    if i == 0:
+        return words + 'way'
+    return words[i:] + words[:i] + 'ay'
+
+def autocorrect(user_input, words_list, score_function):
+    if user_input in words_list:
+        return user_input
+    else:
+        return min(words_list, key = lambda x: score_function(user_input, x))
+
 # END Q1-5
 
 # Question 6
